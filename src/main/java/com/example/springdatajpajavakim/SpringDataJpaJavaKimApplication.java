53069b36dd406f6739b41fa1d33d694c4dd781dc
kimhab <69948342+kimhab11@users.noreply.github.com>
2022-10-11 19:47:39 +0700

    Initial commit

---
+++ src/main/java/com/example/springdatajpajavakim/SpringDataJpaJavaKimApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpajavakim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaJavaKimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaJavaKimApplication.class, args);
+	}
+
+}
